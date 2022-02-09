28a615b855157a1267f56a2c6d21fb315c743d64
user <user@WIN-8NMEU04HFDT>
2022-02-09 15:20:56 +0300

    Добавьте файлы проекта.

---
+++ Калькулятор/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Калькулятор вас приветствует!");
+string x ="";
+int a = 0; //то что получается
+string z = "-";
+x = Console.ReadLine();
+Int32.TryParse(x, out int c);
+x = Console.ReadLine();
+Int32.TryParse(x, out int b);
+z = Console.ReadLine();
+switch (z)
+    {
+    case "-":
+    Console.WriteLine(c,"-",b=a);
+        a = c - b;
+        break;
+    }
